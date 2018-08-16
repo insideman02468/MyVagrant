5b09ec1d272aea227f9f33418d6c18c667843771
vagrant <vagrant@localhost.localdomain>
2018-08-17 01:56:40 +0900

    3_1 is finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_225429) do
+ActiveRecord::Schema.define(version: 2018_08_16_164612) do
 
   create_table "books", force: :cascade do |t|
     t.string "isbn"
